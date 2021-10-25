9ab7434bbeb98de65c549224bfdca28d807bd9a0
Ealyenen <Ealyenen@yandex.ru>
2021-10-26 00:24:57 +0300

    fixed nav

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const eclipse = document.querySelector('.eclipse'),
         background = document.querySelector('.black-back'),
-        moon = document.querySelector('.mobile-menu')
+        moon = document.querySelector('.mobile-menu'),
+        navLink = document.querySelectorAll('.eclipse-link')
 
 eclipse.addEventListener('click', () => {
-    background.classList.toggle('black-back-active')
+    background.classList.toggle('black-back-active'),
     moon.classList.toggle('mobile-menu-active')
 });
 
 background.addEventListener('click', () => {
-    background.classList.remove('black-back-active')
+    background.classList.remove('black-back-active'),
     moon.classList.remove('mobile-menu-active')
+});
+
+navLink.forEach(item => {
+    item.addEventListener('click', function() {
+        background.classList.remove('black-back-active'),
+        moon.classList.remove('mobile-menu-active')
+    });
 });
